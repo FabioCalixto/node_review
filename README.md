b64e6bdb52687c700d1af83ebb5389266989c4a1
FabioCalixto <fabiocalixto16@gmail.com>
2022-08-31 12:16:02 +0100

    metodo get

--- README.md
+++ README.md
@@ -9,3 +9,6 @@ Rodar o projecto
 ### node app.js
 Instalar o modulo para reiniciar o servidor automaticamente
 ###  npm install -g nodemon
+
+Rodar o projecto com o nodemon
+## nodemon app.js
--- app.js
+++ app.js
@@ -6,6 +6,18 @@ app.get("/", (req, res) => {
 
 } );
 
+app.get("/contatos/:id", (req, res) => {
+   const {id} = req.params;
+   const {sit} = req.query;
+    return res.json({
+         id,
+        nome: "Fabio",
+        email: "fabiocalixto16@gmail.com",
+        sit
+    });
+
+} );
+
 app.listen(8080, () => {
 
     console.log("Servidor Inciado na porta 8080");
