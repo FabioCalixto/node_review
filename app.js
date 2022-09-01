52969e12c5f0a02a019e62fe8fa1e10de947fd66
FabioCalixto <fabiocalixto16@gmail.com>
2022-09-01 10:21:44 +0100

    finesh rota post

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Olá Mundo");
@@ -17,6 +18,15 @@ app.get("/contatos/:id", (req, res) => {
     });
 
 } );
+
+app.post("/contatos", (req, res) =>{
+    var {nome} = req.body;
+     var {email} = req.body;
+    return res.json({
+        nome,
+        email
+    });
+});
 
 app.listen(8080, () => {
 
